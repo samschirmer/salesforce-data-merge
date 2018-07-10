3c9d3f5ba1c58a3f7b24f5a4b8ce1ab0b709be2c
Sam Schirmer <samschirmer@gmail.com>
2018-07-10 15:59:59 -0500

    added account handling

--- main.rb
+++ main.rb
@@ -1,12 +1,13 @@
 require 'csv'
 
-class Contact
-	attr_accessor :type, :record
+files = ['./Lead.csv', './Contact.csv']
 
+class Contact
+	attr_accessor :entity_type, :record
 	def initialize(r, file)
-		file == './Lead.csv' ? @type = 'lead' : @type = 'contact'
+		file == './Lead.csv' ? @entity_type = 'lead' : @entity_type = 'contact'
 		# sf gives mailing addresses to contacts, not leads; merging them back together
-		unless @type == 'lead'
+		if @entity_type == 'contact'
 			if r[:street].nil? and r[:city].nil? and r[:state].nil? and r[:postalcode].nil?
 				r[:street] = r[:mailingstreet]
 				r[:city] = r[:mailingcity]	
@@ -21,40 +22,52 @@ class Contact
 				r[:othercountry] = r[:mailingcountry]	
 			end
 		end
-		@row = r
-		@record = @row.to_h
+		@record = r
 	end
 
 	def get_cols
 		blacklist = [	
 			:mailingstreet, :mailingcity, :mailingstate, :mailingpostalcode, :mailingcountry, # because of merge in init
-			:mailingstatecode, :mailingcountrycode, :statecode, :countrycode, :othercountrycode, :otherstatecode
+			:mailingstatecode, :mailingcountrycode, :statecode, :countrycode, :othercountrycode, :otherstatecode,
+			:billingstatecode, :billingcountrycode, :shippingstatecode, :shippingcountrycode
 		]
-		return @row.headers - blacklist
+		return @record.headers - blacklist
 	end
 end
 
-files = ['./Lead.csv', './Contact.csv']
 contacts = Array.new
 files.each do |file|
 	CSV.foreach(File.path(file), headers: true, header_converters: :symbol) do |row|
 		c = Contact.new(row, file)
-		contacts.push(c)
+		contacts.push(c) unless c.record[:isconverted].to_i == 1 && c.entity_type == 'lead'
 	end
 end
 
-lead_cols = contacts.select { |c| c.type == 'lead' }.first.get_cols
-contact_cols = contacts.select { |c| c.type == 'contact' }.first.get_cols
+# merging in account data
+CSV.foreach(File.path('./Account.csv'), headers: true, header_converters: :symbol) do |row|
+	contacts.select { |c| c.record[:accountid] == row[:id] }.each do |c|
+		row.each do |col,v|
+			col = :accountid if	col == :id 
+			col = :accounttype if col == :type
+			c.record[col] = v 
+		end
+	end
+end
+
+lead_cols = contacts.select { |c| c.entity_type == 'lead' }.first.get_cols
+contact_cols = contacts.select { |c| c.entity_type == 'contact' }.first.get_cols
 columns = (lead_cols + contact_cols).uniq
 
 # write to database table at this point
-new_csv = CSV.open('./merged_data.csv', 'w', write_headers: true, headers: columns.push('type'))
+new_csv = CSV.open('./merged_data.csv', 'w', write_headers: true, headers: columns.push('entitytype'))
 
 contacts.each do |c|
 	row = Hash.new
-	row['type'] = c.type
+	row['entitytype'] = c.entity_type
 	columns.each do |col|
 		row[col] = c.record[col] unless c.record[col].nil? || c.record[col] == '[not provided]'
 	end
 	new_csv << row
 end
+
+
